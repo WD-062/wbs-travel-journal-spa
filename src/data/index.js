be2cd7496cc1cd57dff8ef4e9041f5165572a996
Garrett Lander <garrett.jlander@gmail.com>
2025-06-19 17:53:14 +0200

    frontend authorization flow added for ABAC

--- src/data/index.js
+++ src/data/index.js
@@ -1,4 +1,4 @@
-import { createPost, getPosts, getSinglePost } from './posts';
+import { createPost, getPosts, getSinglePost, updatePost, deletePost } from './posts';
 import { me, signin, signup, signout } from './auth';
 
-export { createPost, getPosts, getSinglePost, me, signin, signup, signout };
+export { createPost, getPosts, getSinglePost, me, signin, signup, signout, updatePost, deletePost };
